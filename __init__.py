8108aef118e6374e2ceae3a0920cbea8afc7a61f
dhammawatthumpra-coder <dhammawatthumpra@gmail.com>
2026-05-25 09:08:14 +0700

    Bump version to 0.1.1

--- __init__.py
+++ __init__.py
@@ -14,7 +14,7 @@ from .dynamics import (
 )
 from .analytics import TheoryGuidedClustering, VisualizationAPI
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = [
     'TransformerAdapter',
